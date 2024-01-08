172d685aaf3064324d0d0e09081ec853ef030f78
Leonardo Massazza <lmassazza@gmail.com>
2024-01-08 15:59:56 -0300

    add endorsed

--- contracts/PerpsV2RiskControlModule.sol
+++ contracts/PerpsV2RiskControlModule.sol
@@ -17,20 +17,23 @@ contract PerpsV2RiskControlModule is Ownable {
   address public constant SNX_PDAO_MULTISIG_ADDRESS = 0x6cd3f878852769e04A723A5f66CA7DD4d9E38A6C;
   address public constant SNX_ADDRESS_RESOLVER = 0x95A6a3f44a70172E7d50a9e28c85Dfd712756B8C;
 
+  address public endorsed;
+
   GnosisSafe private _pDAOSafe;
   AddressResolver private _addressResolver;
 
   bool public isPaused;
 
-  constructor(
-    address _owner,
-  ) {
+  constructor(address _owner, address _endorsed) {
     // Do not call Ownable constructor which sets the owner to the msg.sender and set it to _owner.
     _transferOwnership(_owner);
 
     // contracts
     _addressResolver = AddressResolver(SNX_ADDRESS_RESOLVER);
     _pDAOSafe = GnosisSafe(SNX_PDAO_MULTISIG_ADDRESS);
+
+    // endorsed
+    endorsed = _endorsed;
 
     // start as not paused
     isPaused = false;
@@ -41,6 +44,7 @@ contract PerpsV2RiskControlModule is Ownable {
   // @dev set MMV to zero on the corresponding market.
   function coverRisk(bytes32 markeKey) external returns (bool success) {
     require(!isPaused, 'Module paused');
+    require(msg.sender == endorsed, 'Not endorsed')
 
     success = _executeSafeTransaction(markeKey);
   }
@@ -50,10 +54,19 @@ contract PerpsV2RiskControlModule is Ownable {
     isPaused = _isPaused;
   }
 
+  // @dev sets the paused state
+  function setEndorsed(address _endorsed) external onlyOwner {
+    endorsed = _endorsed;
+  }
+
   // --- Internal --- //
 
   function _executeSafeTransaction(bytes32 markeKey) internal returns (bool success) {
-    bytes memory payload = abi.encodeWithSignature('setMaxMarketValue(bytes32,uint256)', markeKey, 0);
+    bytes memory payload = abi.encodeWithSignature(
+      'setMaxMarketValue(bytes32,uint256)',
+      markeKey,
+      0
+    );
     address marketSettingsAddress = _addressResolver.requireAndGetAddress(
       'PerpsV2MarketSettings',
       'Missing Perpsv2MarketSettings address'
@@ -78,4 +91,6 @@ contract PerpsV2RiskControlModule is Ownable {
 
   event ReduceMMVDone(bytes32 markeKey, uint256 paramterUpdatedAtTime);
   event ReduceMMVFailed(bytes32 markeKey, uint256 paramterUpdateAttemptedAtTime);
+
+  error NotEndorsed(address caller);
 }
